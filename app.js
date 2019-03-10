9fc4cd011744b8d94e12f3b3e74ef68b699e1c08
Ankur Vishwakarma <avishwakarma@anant.saama.com>
2019-03-10 20:15:05 +0530

    add code changes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ProjectRoute = require("./api/routes/ProjectRoute");
 require('./mongo.config');
 
 app.use((req,res,next)=>{
+    console.log(req.headers)
       // Website you wish to allow to connect
       res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -15,8 +16,8 @@ app.use((req,res,next)=>{
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   
       // Request headers you wish to allow
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
+      res.setHeader('Access-Control-Allow-Headers', '*');
+  //X-Requested-With,Content-Type,authorization
       // Set to true if you need the website to include cookies in the requests sent
       // to the API (e.g. in case you use sessions)
       res.setHeader('Access-Control-Allow-Credentials', true);
